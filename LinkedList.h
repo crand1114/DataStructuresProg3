2f3041f6040cc894581cf13c3e80f1d3b9085315
Charles Randall <ccrandal@aggies.ncat.edu>
2018-02-24 02:52:22 -0500

    not complete

--- LinkedList.h
+++ LinkedList.h
@@ -1,3 +1,4 @@
+#include <string>
 using namespace std;
 typedef string ElementType;
 
@@ -16,10 +17,10 @@ public:
     void ReverseDisplay(); // Display the values in the LinkedList in reverse (do not reverse the actual LinkedList)
     void GetValueAt(int n); // Return the value at the provided location n
     void DeleteAll(ElementType x); // If value x is in the LinkedList, remove all instances of x
-    void Display(string type); // Display the strings in the LinkedList based on the user's specification  (all of them, the even length ones, or the odd length ones).
-    void Sort(); // Sort the LinkedList of strings based on length (shortest to longest)
-    double Median(); // Compute and return the median string length for the strings in the LinkedList (must be sorted)
-    void Shuffle(); // Shuffle the strings in the LinkedList around
+   // void Display(string type); // Display the strings in the LinkedList based on the user's specification  (all of them, the even length ones, or the odd length ones).
+   // void Sort(); // Sort the LinkedList of strings based on length (shortest to longest)
+    //double Median(); // Compute and return the median string length for the strings in the LinkedList (must be sorted)
+    //void Shuffle(); // Shuffle the strings in the LinkedList around
 
 private:
     Node * first; // Pointer to first Node of LinkedList
